7ecefe8db1520a5ced2fc9783c5fec8605976cbd
Xavier Perez <duosrx@gmail.com>
2020-09-02 16:10:44 -0700

    Add EI and LdWriteIoC

--- src/memory.rs
+++ src/memory.rs
@@ -26,17 +26,17 @@ impl Memory {
 
     pub fn load(&mut self, address: u16) -> u8 {
         match address {
-            0xFF40 => self.gpu.control,
-            0xFF42 => self.gpu.scroll_y,
-            0xFF43 => self.gpu.scroll_x,
-            0xFF44 => self.gpu.ly,
-            0xFF47 => self.gpu.bg_palette,
             0x0000..=0x7FFF => self.cartridge.rom[address as usize],
             0x8000..=0x9FFF => self.gpu.load(address),
             0xC000..=0xDFFF => self.work_ram[(address & 0x1FFF) as usize],
             0xE000..=0xFDFF => self.work_ram[((address - 0x2000) & 0x1FFF) as usize],
             0xFE00..=0xFE9F => 0, // TODO: OAM
             0xFEA0..=0xFEFF => 0, // No-op
+            0xFF40 => self.gpu.control,
+            0xFF42 => self.gpu.scroll_y,
+            0xFF43 => self.gpu.scroll_x,
+            0xFF44 => self.gpu.ly,
+            0xFF47 => self.gpu.bg_palette,
             0xFF00..=0xFF7F => self.io[address as usize - 0xFF00],
             0xFF80..=0xFFFF => self.high_ram[address as usize - 0xFF80],
             _ => unimplemented!("Loading {:04X}", address),
@@ -51,18 +51,18 @@ impl Memory {
         }
         match address {
             // 0x0000..=0x7FFF => self.cartridge.rom[address as usize] = value,
-            // 0xFF40 => { println!("{:08b}", value); self.gpu.control = value },
+            0x0000..=0x7FFF => {} // TODO: Cartridge ram, MBC...etc
+            0x8000..=0x9FFF => self.gpu.store(address, value),
+            0xC000..=0xDFFF => self.work_ram[(address & 0x1FFF) as usize] = value,
+            0xE000..=0xFDFF => self.work_ram[((address - 0x2000) & 0x1FFF) as usize] = value,
+            0xFE00..=0xFE9F => self.gpu.oam_store(address & 0x9F, value),
+            0xFEA0..=0xFEFF => {} // No-op
             0xFF40 => self.gpu.control = value,
             0xFF42 => self.gpu.scroll_y = value,
             0xFF43 => self.gpu.scroll_x = value,
             0xFF44 => self.gpu.ly = 0,
             0xFF46 => { dbg!("DMA"); }, // TODO
             0xFF47 => self.gpu.bg_palette = value,
-            0x8000..=0x9FFF => self.gpu.store(address, value),
-            0xC000..=0xDFFF => self.work_ram[(address & 0x1FFF) as usize] = value,
-            0xE000..=0xFDFF => self.work_ram[((address - 0x2000) & 0x1FFF) as usize] = value,
-            0xFE00..=0xFE9F => self.gpu.oam_store(address & 0x9F, value),
-            0xFEA0..=0xFEFF => {} // No-op
             0xFF00..=0xFF7F => self.io[address as usize - 0xFF00] = value,
             0xFF80..=0xFFFF => self.high_ram[address as usize - 0xFF80] = value,
             _ => unimplemented!("Storing {:02X} @ {:04X}", value, address),
