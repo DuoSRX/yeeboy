bf15b38680085969eb0c406a64c37b1797a9dd8f
Xavier Perez <duosrx@gmail.com>
2020-09-09 16:28:09 -0700

    Add experiment wasm frontend

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,13 @@ pub mod opcodes;
 pub mod memory;
 pub mod register;
 pub mod timer;
+
+pub fn blah() -> String {
+    "Foo".into()
+}
+
+        // let mut rom = Vec::new();
+        // file.read_to_end(&mut rom).expect("Cannot read file");
+// use std::fs::File;
+// use std::io::prelude::*;
+
